c350036e11686603290dadb3a54d740588e21f7c
Dominik Croci <dominikrcroci@gmail.com>
2023-09-08 14:04:09 -0700

    css updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="../trollface.png" className="header--image" />
+      <img src="src/trollface.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
     </header>
   );
